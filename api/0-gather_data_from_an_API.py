cacd00511279b1a0470c86b10e6a2fd817bba343
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-13 03:31:36 +0100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,14 +2,14 @@
 
 """Gather data from an API"""
 
-
 import requests
 import json
 import sys
 
 
 id = sys.argv[1]
-user_response = requests.get("https://jsonplaceholder.typicode.com/users/"+id)
+user_response = requests.get(
+    "https://jsonplaceholder.typicode.com/users/" + id)
 todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
 
 
@@ -27,8 +27,17 @@ for i in todos_data:
             title_task.append(i["title"])
 
 
-print("Employee {} is done with tasks ({}/{}):".format(
-    user_response.json().get('name'), len(title_task), nbr_task))
+"""
+dc_names = {k: v for d in user_data for k, v in d.items()}
+dc_names = {}
+for d in range(len(user_data)):
+    dc_names.update(user_data[d])
+print(user_data[4])
+"""
+
+
+print(
+    f"Employee {user_response.json().get('name')} is done with tasks ({len(title_task)}/{nbr_task}):")
 
 for i in title_task:
-    print("\t{}".format(i))
+    print(f"\t{i}")

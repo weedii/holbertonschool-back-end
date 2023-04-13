51ccc10cff502f17a6c6ad48ab8d7ea039cbe5f7
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-13 11:27:57 +0100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
     print(
         f"Employee {user_response.json().get('name')}"
-        f"is done with tasks({len(title_task)}/{nbr_task}):")
+        f" is done with tasks({len(title_task)}/{nbr_task}):")
 
     for i in title_task:
         print(f"\t {i}")

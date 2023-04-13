907f73def2a6975ef7dfd872be376dbfa0b4f61d
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-14 00:12:49 +0100

    Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Gather data from an API"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+
+    id = sys.argv[1]
+    response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{id}/todos")
+    todos = response.json()
+
+    completed_tasks = [todo for todo in todos if todo["completed"]]
+
+    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
+    employee_name = response.json()["username"]
+
+    filename = f"{id}.csv"
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        for i in todos:
+            writer.writerow([(i["userId"]), employee_name,
+                            i["completed"], i["title"]])
